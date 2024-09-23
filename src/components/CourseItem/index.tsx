1e42ea598869ac71a760cde3e1953e3593c4fa22
Leandro Atallah <leandroatallah@gmail.com>
2024-09-23 13:11:31 -0300

    chore: add icon to buttons

--- src/components/CourseItem/index.tsx
+++ src/components/CourseItem/index.tsx
@@ -42,8 +42,21 @@ const CourseItem = ({ data, onDeleteCourse }: CourseItemProp) => {
       <div>
         <Button
           type="button"
+          icon={
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 448 512"
+              height="12px"
+              width="12px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"></path>
+            </svg>
+          }
           style={{
-            padding: 4,
+            padding: 6,
             fontSize: 12,
             height: "auto",
           }}
@@ -54,7 +67,10 @@ const CourseItem = ({ data, onDeleteCourse }: CourseItemProp) => {
         </Button>
       </div>
       {showConfirmDeletion && (
-        <Modal title="Remover curso">
+        <Modal
+          title="Remover curso"
+          onClose={() => setShowConfirmDeletion(false)}
+        >
           <div>Tem certeza que deseja remover o curso?</div>
           <Button onClick={onDeleteCourse}>Remover curso</Button>
           <Button
